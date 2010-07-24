4a3518536090d464ab2ccca958c7e28f993862af
martin.avanzini <martin.avanzini@uibk.ac.at>
2010-07-24 11:28:55 +0200

    added source/Main.hs
    Ignore-this: f673849deb09f66370ae4b534e2bfd5f
    
    darcs-hash:20100724092855-3c1c7-a15e2b540320ed1377001eb7c6ee9370e19d4fae.gz

---
+++ source/Main.hs
@@ -0,0 +1,23 @@
+{-
+This file is part of the Tyrolean Complexity Tool (TCT).
+
+The Tyrolean Complexity Tool is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+The Tyrolean Complexity Tool is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the Tyrolean Complexity Tool.  If not, see <http://www.gnu.org/licenses/>.
+-}
+
+
+module Main where
+import Tct.Main 
+
+main :: IO ()
+main = tct defaultConfig

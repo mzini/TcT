a048b527efa970ead0d5309219c1b621e986daac
Martin Avanzini <martin.avanzini@uibk.ac.at>
2011-05-23 09:04:44 +0200

    UsableRules: shorter output when no usable rules

--- source/Tct/Method/DP/UsableRules.hs
+++ source/Tct/Method/DP/UsableRules.hs
@@ -70,14 +70,14 @@ data URProof = URProof { usableStrict :: Trs
                | Error DPError
 
 instance PrettyPrintable URProof where 
-    pprint p@URProof {} | null all        = text "No rule is usable."
+    pprint p@URProof {} | null allUrs     = text "No rule is usable."
                         | progressed p    = text "We replace strict/weak-rules by the corresponding usable rules:"
                                             $+$ text ""
                                             $+$ indent (ppTrs "Strict Usable Rules" (usableStrict p)
                                                        $+$ ppTrs "Weak Usable Rules" (usableWeak p))
                         | otherwise       = text "All rules are usable."
-        where ppTrs = pprintNamedTrs (signature p) (variables p)
-              all   = Trs.rules (usableStrict p) ++ Trs.rules (usableWeak p)
+        where ppTrs  = pprintNamedTrs (signature p) (variables p)
+              allUrs = Trs.rules (usableStrict p) ++ Trs.rules (usableWeak p)
     pprint (Error e)                    = pprint e
 
 instance T.TransformationProof UR where

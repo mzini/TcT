76cc14cad025de7cacbd710264759359fafbbd20
Martin Avanzini <martin.avanzini@uibk.ac.at>
2011-07-18 17:12:56 +0200

    removed compiler warning

--- source/Tct/Method/Custom.hs
+++ source/Tct/Method/Custom.hs
@@ -79,7 +79,7 @@ processor d f = S.StdProcessor CP {description = toDescription d, code = f }
 
 strategy :: (P.Processor proc, IsDescription d arg) =>
            (A.Domains arg -> P.InstanceOf proc) -> d -> CustomProcessor arg P.SomeProof
-strategy mkinst d = processor d (\ as prob -> P.SomeProof `liftM` (P.solve (mkinst as) prob))
+strategy mkinst d = processor d (\ sargs prob -> P.SomeProof `liftM` (P.solve (mkinst sargs) prob))
 
 processorFromInstance :: (IsDescription d arg, P.Processor proc) => 
              d -> (A.Domains arg -> P.InstanceOf proc) -> (CustomProcessor arg (P.ProofOf proc))

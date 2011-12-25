8a5987aa11f42548001b85762a4d85fba1b7c109
Martin Avanzini <martin.avanzini@uibk.ac.at>
2011-12-25 21:41:32 +0900

    added Tct.Configuration

---
+++ source/Tct/Configuration.hs
@@ -0,0 +1,16 @@
+module Tct.Configuration where
+
+--------------------------------------------------------------------------------
+-- | 
+-- Module      :  Tct.Configuration
+-- Copyright   :  (c) Martin Avanzini <martin.avanzini@uibk.ac.at>, 
+--                Georg Moser <georg.moser@uibk.ac.at>, 
+--                Andreas Schnabl <andreas.schnabl@uibk.ac.at>,
+-- License     :  LGPL (see COPYING)
+--
+-- Maintainer  :  Martin Avanzini <martin.avanzini@uibk.ac.at>
+-- Stability   :  unstable
+-- Portability :  unportable      
+-- 
+--------------------------------------------------------------------------------   
+

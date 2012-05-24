48e6e3b42ce0bf789a882c7bc10125844019a360
Martin Avanzini <martin.avanzini@uibk.ac.at>
2012-05-24 11:24:29 +0200

    fixed missing pattern match in naturalMI.hs

--- source/Tct/Method/Matrix/NaturalMI.hs
+++ source/Tct/Method/Matrix/NaturalMI.hs
@@ -276,7 +276,9 @@ usableArgsWhereApplicable MNoDP   sig Prob.BasicTerms {} ua strat r = if ua then
 catchException :: P.SolverM m => m (S.ProofOf NaturalMI) -> m (S.ProofOf NaturalMI)
 catchException m = 
   do io <- P.mkIO m 
-     liftIO $ io `E.catch` (\ E.StackOverflow -> return $ ExceptionRaised E.StackOverflow)
+     liftIO $ E.catchJust stackOverflow io (const $ return $ ExceptionRaised E.StackOverflow)
+  where stackOverflow E.StackOverflow = Just ()
+        stackOverflow _               = Nothing
 
 
 orientRelative :: P.SolverM m => Prob.Strategy -> Prob.StartTerms -> Trs.Trs -> Trs.Trs -> F.Signature -> Domains (S.ArgumentsOf NaturalMI) -> m (S.ProofOf NaturalMI)

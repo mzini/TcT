c133e21cae45a5142fc612a51d18bb0542158ae6
Martin Avanzini <martin.avanzini@uibk.ac.at>
2012-05-24 09:54:23 +0200

    added exception constructor to Ordering

--- source/Tct/Processor/Orderings.hs
+++ source/Tct/Processor/Orderings.hs
@@ -19,32 +19,37 @@ module Tct.Processor.Orderings
     )
 where
 
+import Termlib.Utils (paragraph)  
 import Text.PrettyPrint.HughesPJ
 import Tct.Certificate (constant, certified)
 import Tct.Processor (Answer (..), ComplexityProof(..))
 import Tct.Utils.PPrint (indent)
+import qualified Control.Exception as C
 import qualified Tct.Utils.Xml as Xml
 
 data OrientationProof o = Order o -- ^ Proven with given order.
                         | Incompatible -- ^ The problem is incompatible.
                         | Empty  -- ^ The problem is empty
                         | Inapplicable String -- ^ Inapplicable for given reason.
+                        | ExceptionRaised C.AsyncException -- ^ Some exception was raised.
   deriving Show
 
 instance ComplexityProof o => ComplexityProof (OrientationProof o) where
-    pprintProof Empty            _  = text "All strict components are empty, nothing to further orient"
-    pprintProof (Order o)        mde = pprintProof o mde
-    pprintProof Incompatible     _   = text "The input cannot be shown compatible"
-    pprintProof (Inapplicable s) _   = text "The processor is inapplicable, reason:"
-                                       $+$ indent (text s)
+    pprintProof Empty            _    = text "All strict components are empty, nothing to further orient"
+    pprintProof (Order o)        mde  = pprintProof o mde
+    pprintProof Incompatible     _    = text "The input cannot be shown compatible"
+    pprintProof (Inapplicable s) _    = text "The processor is inapplicable, reason:"
+                                        $+$ indent (text s)
+    pprintProof (ExceptionRaised e) _ = text "Following exception was raised:"
+                                        $+$ indent (paragraph $ show e)
 
     answer (Order o) = answer o
     answer Empty     = CertAnswer $ certified (constant, constant)
-    answer Incompatible = MaybeAnswer
-    answer (Inapplicable _) = MaybeAnswer
+    answer _ = MaybeAnswer
 
     toXml ord = Xml.elt "order" [] [toXml' ord]
       where toXml' (Order o) = Xml.elt "compatible" [] [toXml o]
             toXml' Incompatible = Xml.elt "incompatible" [] []
             toXml' Empty = Xml.elt "empty" [] []    
             toXml' (Inapplicable r) = Xml.elt "inapplicable" [] [Xml.text r]    
+            toXml' (ExceptionRaised e) = Xml.elt "exceptionRaised" [] [Xml.text $ show e]

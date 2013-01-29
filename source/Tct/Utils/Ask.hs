d3b4e399a6e007119b80b23140cb1a3d20ce323b
Martin Avanzini <martin.avanzini@uibk.ac.at>
2013-01-29 23:20:52 +0100

    removed commented code

--- source/Tct/Utils/Ask.hs
+++ source/Tct/Utils/Ask.hs
@@ -15,24 +15,13 @@
 module Tct.Utils.Ask 
        (
          ask
-       -- , clearHistory
        , askStr
        )
        where
 
-
--- import System.Console.Readline
 import System.Console.Haskeline
 import Data.List (isPrefixOf)
-import Control.Monad.Trans (MonadIO, liftIO)
 
--- foreign import ccall "readline/history.h clear_history" clearHistory :: IO ()
-
--- -- prompt :: String
--- -- prompt = " > "
-
--- completeFromList :: (Monad m, Eq a) => [[a]] -> Maybe ([a] -> m [[a]])
--- completeFromList lst = Just $ \ s -> return [s' | s' <- lst , s `isPrefixOf` s' ]
 
 completeFromList :: MonadException m => [String] -> String -> m [Completion]
 completeFromList lst str = return [simpleCompletion s | s <- lst, str `isPrefixOf` s]
@@ -43,11 +32,6 @@ ask prompt options rd = do
   return $ mr >>= rd
   where
     completeFN = completeWord Nothing " \t" $ completeFromList options
--- ask l rd = do 
---   liftIO initialize
---   liftIO $ setCompletionEntryFunction (completeFromList l)
---   mr <- liftIO $ readline ""
---    return $ mr >>= rd
 
 askStr :: MonadException m => String -> [String] -> m (Maybe String)
 askStr prompt options = ask prompt options rd 
